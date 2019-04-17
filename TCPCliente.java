33fe801341a1b6f5ca54b3ac12fb6fba5e6853b2
RaizerOkisama <38229718+RaizerOkisama@users.noreply.github.com>
2019-04-17 13:33:08 -0300

    Adicionando arquivos no repositório

---
+++ TCPCliente.java
@@ -0,0 +1,77 @@
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import javax.swing.JOptionPane;
+/**
+ *
+ * DOCUMENTA��O DA CLASSE ---------------------- FINALIDADE: Classe que
+ * implementa a parte cliente de um Socket TCP.
+ *
+ * HIST�RICO DE DESENVOLVIMENTO: Marco 18, 2019 - @author Alexandre A. Amaral
+ * - @version 1.0.
+ *
+ */
+public class TCPCliente {
+
+	private ObjectInputStream entrada;
+	private ObjectOutputStream saida;
+	private Socket s;
+
+	/**
+	 * Construtor default da classe.
+	 * 
+	 * @author Alexandre A. Amaral.
+	 */
+	public TCPCliente() {
+
+		try {
+			String ip = JOptionPane.showInputDialog(null, "Digite o IP do servidor: ");
+			while (ip.isEmpty()) {
+				System.out.println("Digite o endereço IP para realizar a conexão com o servidor.");
+				ip = JOptionPane.showInputDialog(null, "Digite o IP do servidor: ");
+			}
+
+			try {
+				s = new Socket(ip, 5000);
+				System.out.println(">>| A conexao com o servidor foi realizada com sucesso |<<");
+				System.out.println(">>| Info. servidor - " + s.getRemoteSocketAddress() + " |<<");
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("ERRO: Nao foi possivel estabelecer uma conexao com o servidor.");
+			}
+			saida = new ObjectOutputStream(s.getOutputStream());
+			entrada = new ObjectInputStream(s.getInputStream());
+			String texto = "";
+			do {
+				try {
+					// envia uma mensagem para o servidor
+					texto = JOptionPane.showInputDialog(null, "Digite o nome do programa que deseja executar: ");
+					saida.writeObject(texto);
+					saida.flush();
+					// l� os dados recebidos do servidor
+					System.out.println(entrada.readObject());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} while(!texto.equalsIgnoreCase("SAIR"));	
+			// finaliza a conex�o e fecha os streams e sockets
+			entrada.close();
+			saida.close();
+			s.close();			
+		} catch(Exception e) {
+			e.printStackTrace();
+		} 
+		
+	}
+
+	/**
+	 * M�dodo principal.
+	 * 
+	 * @author Alexandre A. Amaral.
+	 * @param args
+	 */    
+	public static void main (String args[]) {
+		new TCPCliente();
+	} 
+	
+}
---
+++ TCPServerMultiCliente.java
@@ -0,0 +1,144 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+/**
+ *
+ * DOCUMENTA��O DA CLASSE 
+ * ---------------------- 
+ * FINALIDADE: Classe que implementa a parte servidor de um Socket
+ * TCP. Aceita diversos clientes simultanemante.
+ *
+ * HIST�RICO DE DESENVOLVIMENTO: 
+ * Marco 18, 2019 - @author Alexandre A. Amaral -  @version 1.0.
+ *
+ */
+public class TCPServerMultiCliente {
+
+	private ServerSocket ss;
+	private ObjectInputStream entrada; 
+	private ObjectOutputStream saida; 
+
+	/**
+	 * Construtor default da classe.
+	 * 
+	 * @author Alexandre A. Amaral.
+	 */    
+	public TCPServerMultiCliente() { 
+
+		try { 
+			this.ss = new ServerSocket(5000); 
+
+			while(true){
+				// aceita as conex�es 
+				Socket clientSocket = ss.accept();
+	
+				// inicia a thread para o cliente  
+				new Thread(new ClienteThread(clientSocket)).start();
+			}			 
+
+		} 
+		catch(Exception e) { 
+			e.printStackTrace();
+		}finally{
+			try {
+				this.ss.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	} 
+
+
+	/**
+	 *
+	 * DOCUMENTA��O DA CLASSE
+	 * ----------------------
+	 * FINALIDADE:
+	 * Inner class que tem a finalidade de tratar a conex�o com cada cliente.
+	 *
+	 * HIST�RICO DE DESENVOLVIMENTO:
+	 * Mar�o 18, 2019 - @author Alexandre A. Amaral - Primeira vers�o da classe.
+	 *
+	 */	
+	class ClienteThread implements Runnable {  
+
+		private Socket novoCliente;
+		
+		/**
+		 * Construtor default da classe.
+		 * 
+		 * @author Alexandre A. Amaral.
+		 */   		
+		public ClienteThread(Socket s) { 
+			this.novoCliente = s; 
+		} 
+
+		@Override
+		public void run() {            
+
+			// obt�m o identificador da Thread
+			long idThread = Thread.currentThread().getId();            
+
+			// mostra informa��es detalhadas da conex�o  
+			System.out.println("Cliente conectado - " + this.novoCliente.getRemoteSocketAddress()); 
+
+			try {
+
+				entrada = new ObjectInputStream(this.novoCliente.getInputStream()); 
+				saida = new ObjectOutputStream(this.novoCliente.getOutputStream()); 
+
+				Object valor;
+				// fica em loop at� a Thread do cliente for falsa (fechar a conex�o) 
+				do {                  
+					// l� entrada
+					valor = entrada.readObject();
+					if (valor.toString().isEmpty()){
+						saida.writeObject("Servidor(" + idThread + "): Digite o nome do programa que você deseja executar.");
+						saida.flush();
+					} else {
+						try {
+							// Abre o processo
+							ProcessBuilder builder = new ProcessBuilder((String) valor);
+							Process pro = builder.start();
+							// envia a resposta para o cliente
+							saida.writeObject("O programa " + valor.toString().toUpperCase() + " foi executado com sucesso !");
+						} catch (Exception e){
+							saida.writeObject("Nao foi possivel executar o programa informado, tente novamente.");
+							e.printStackTrace();
+						} finally {
+							saida.flush(); 	
+						}
+					}
+				} while(!valor.toString().equalsIgnoreCase("SAIR")); 
+			} 
+			catch(Exception e) { 
+				e.printStackTrace(); 
+			} 
+			finally { 
+				try {                    
+					// fecha a conex�o
+					entrada.close(); 
+					saida.close(); 
+					novoCliente.close(); 	                    
+				} 
+				catch(IOException ioe) { 
+					ioe.printStackTrace(); 
+				} 
+			} 
+		} 
+
+	} // fim inner class
+	/**
+	 * M�dodo principal.
+	 * 
+	 * @author Alexandre A. Amaral.
+	 * @param args
+	 */    
+	public static void main (String args[]) {
+		new TCPServerMultiCliente();
+	} 
+	
+}
